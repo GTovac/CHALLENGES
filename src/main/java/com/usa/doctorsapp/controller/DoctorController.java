5f53d0e5ce22601da4a6a0fdc6049843319c1df3
GTovac <german.tovar.mt@usa.edu.co>
2022-10-15 11:58:57 -0500

    arreglo doctor controller

--- src/main/java/com/usa/doctorsapp/controller/DoctorController.java
+++ src/main/java/com/usa/doctorsapp/controller/DoctorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/Doctor")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class DoctorController {
 
